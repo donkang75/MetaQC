7c1a24ddf762a4a923acd100bdae499ab0b5f3f1
Don Kang <donkang75@gmail.com>
2012-02-25 20:57:55 -0800

    deprecated cleanup function

--- R/MetaQC.R
+++ R/MetaQC.R
@@ -55,8 +55,10 @@ MetaQC <- function(DList, GList, isParallel=FALSE, nCores=NULL, useCache=TRUE, f
 				}
 				
 				Cleanup <- function(.) {
-					if(!is.null(.$.workers))
-						stopWorkers(.$.workers)
+					warning("This function was deprecated.\n",
+							"No more needed to call this function.\n")
+#					if(!is.null(.$.workers))
+#						stopWorkers(.$.workers)
 				}
 				
 				.Initialize <- function(., .DList, .GList, .filterGenes) {
